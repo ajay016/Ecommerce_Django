dfdd23637d1f541e06ba7614f04a1daf035a4d09
Ajay <ajayghosh28@gmail.com>
2022-11-25 23:37:50 +0600

    created the cookie function in utils.py and removed cookie code from the view.py

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 import datetime
 import json
 from .models import *
+from .utils import cookieCart
 
 def store(request):
     
@@ -12,27 +13,32 @@ def store(request):
     # if the user is authenticated, return the no. of item in the acrt and the cart total
     # else return empty cart and a cart  total of 0
     if request.user.is_authenticated:
-        try:
-            # if the user is authenticated, assign the user to a customer
-            customer = request.user.customer
-            order, created = Order.objects.get_or_create(customer=customer, complete=False)
-            items = order.orderitem_set.all()
+        # try:
+        # if the user is authenticated, assign the user to a customer
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        
+        # showing the item quantity in the cart icon, not an efficient way to do it
+        cartItems = order.get_cart_items
             
-            # showing the item quantity in the cart icon, not an efficient way to do it
-            cartItems = order.get_cart_items
-            
-        except:
-            items = []
-            order = {'get_cart_total': 0, 'get_cart_items': 0}
-            cartItems = order['get_cart_items']
+        # except:
+    else:
+        # items = []
+        # order = {'get_cart_total': 0, 'get_cart_items': 0}
+        # cartItems = order['get_cart_items']
+        cookieData = cookieCart(request)
+        order = cookieData['order']
+        cartItems = cookieData['cartItems']
+        
             
     # if the user is not authenticated the cart item shows 0
-    else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+    # else:
+    #     items = []
+    #     order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         
-        # cartItems shows the item quantity in the cart icon, not an efficient way to do it
-        cartItems = order['get_cart_items']
+    #     # cartItems shows the item quantity in the cart icon, not an efficient way to do it
+    #     cartItems = order['get_cart_items']
         
     # cartItems shows the item quantity in the cart icon, not an efficient way to do it
     context = {'products': products, 'order': order, 'cartItems': cartItems}
@@ -53,63 +59,10 @@ def cart(request):
         # shows the total number of items in the cart
         
     else:
-        # if the user is not authenticated
-        # get the cookies from the website. The name of the cookie was 'cart'
-        # All the cookies is in json format. Convert it in python dictionary using json.loads
-        # Get all the cookies using 'request.cookies['cart]', here name of the cookie is '[cart]'
-        try:
-            cart = json.loads(request.COOKIES['cart'])
-            print('Cart:', cart)
-        except:
-            cart = {}
-            
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0 , 'shipping': False}
-        
-        # cartItems shows the item quantity in the cart icon, not an efficient way to do it
-        cartItems = order['get_cart_items']
-        
-        # loop through the cart dictionary, cart contains "2: '{quantity: 1}'", where '2' is the productID
-        # and '{quantity: 1}' is dictionary within the cart dictionary containig the quantity of the product
-        for i in cart:
-            # Try to get the product by its "id", if it is removed return empty cart
-            try:
-                # 'cart[i]["quantity"]' returns the quantity of the product
-                # we get the quantity of the from the cookie set in the javascript and called here by 'cart = json.loads(request.COOKIES['cart'])'
-                # So, for an unauthenticated user quantitiy of products in the cart is zero as set in the "order"
-                # "cartItems" which was "0" to the value of the quantity that we got from the cookie
-                cartItems += cart[i]["quantity"]
-                
-                # Get the product id from the cookie
-                product = Product.objects.get(id=i)
-                total = (product.price * cart[i]["quantity"])
-                
-                # Get the total amount of the cart
-                order['get_cart_total'] += total
-                
-                # Get the total number of items in the cart
-                order['get_cart_items'] += cart[i]["quantity"]
-                
-                # render the items for unauthenticated user to display n the cart page
-                item = {
-                    'product': {
-                        'id': product.id,
-                        'name': product.name,
-                        'price': product.price,
-                        'imageURL': product.imageURL
-                    },
-                    'quantity': cart[i]["quantity"],
-                    'get_total': total
-                }
-                
-                items.append(item)
-                
-                #print('Order:', order)
-                if product.digital == False:
-                    order['shipping'] = True
-            
-            except:
-                pass
+        cookieData = cookieCart(request)
+        items = cookieData['items']
+        order = cookieData['order']
+        cartItems = cookieData['cartItems']
         
     # cartItems shows the item quantity in the cart icon, not an efficient way to do it
     context = {'items': items, 'order': order, 'cartItems': cartItems}    
@@ -130,12 +83,19 @@ def checkout(request):
         # shows the total number of items in the cart
         
     else:
-        items = []
-        customer = {}
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+        # if the user is unauthenticated return empty cart items and order
+        # items = []
+        # customer = {}
+        # order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         
         # cartItems shows the item quantity in the cart icon, not an efficient way to do it
-        cartItems = order['get_cart_items']
+        # cartItems = order['get_cart_items']
+        
+        # if the user is unauthenticated return the items, order and cartItems from the cookie that we got through javascript in utils.py file
+        cookieData = cookieCart(request)
+        items = cookieData['items']
+        order = cookieData['order']
+        cartItems = cookieData['cartItems']
         
     context = {'items': items, 'order': order, 'cartItems': cartItems, 'customer': customer}
     return render(request, 'store/checkout.html', context)
